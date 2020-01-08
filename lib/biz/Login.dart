650e17906c64dbafcfe10a0f6fc1c5e0d3993be6
苏震 <suzhen@viewhigh.com>
2020-01-08 14:37:38 +0800

    Fluttertoast
    Fluttertoast

---
+++ lib/biz/Login.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'dart:convert' as convert;
+
+class Login extends StatelessWidget {
+  Login({Key key}) : super(key: key);
+  final GlobalKey _formKey = new GlobalKey<FormState>();
+  // 赋值给某个元素
+  final TextEditingController _passwordController = new TextEditingController();
+  final TextEditingController _userNameController = new TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: AppBar(
+        title: new Text('易普优采'),
+        centerTitle: true,
+      ),
+      body: new SingleChildScrollView(
+        physics: BouncingScrollPhysics(),
+        child: new Center(
+          child: new Column(
+            children: <Widget>[
+              Container(
+                child: Image.asset('static/image/epro.jpg'),
+              ),
+              Container(
+                  child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    TextFormField(
+                      autofocus: true,
+                      controller: _userNameController,
+                      decoration: InputDecoration(
+                          labelText: 'User name',
+                          hintText: 'User name',
+                          icon: Icon(Icons.perm_contact_calendar)),
+                      validator: (val) {
+                        if (val.trim().length < 6) {
+                          return 'Not enough length';
+                        }
+                        return val.trim().length > 0
+                            ? null
+                            : 'User name is required';
+                      },
+                    ),
+                    TextFormField(
+                      autofocus: false,
+                      controller: _passwordController,
+                      decoration: InputDecoration(
+                          labelText: 'password',
+                          hintText: 'Please input password',
+                          icon: Icon(Icons.remove_red_eye)),
+                      obscureText: true,
+                    ),
+                    RaisedButton(
+                      child: Text('Login'),
+                      color: Colors.blue,
+                      textColor: Colors.white,
+                      onPressed: () async {
+                        if ((_formKey.currentState as FormState).validate()) {
+                          // Get parameters
+                          var data = {
+                            "identifier": _userNameController.text,
+                            "password": _passwordController.text,
+                            "captchaCode": ""
+                          };
+                          Dio dio = new Dio();
+                          // parameter 1: request path
+                          // parameter 2: request parameters
+                          Response response = await dio.post(
+                              'https://epro-hospital.test.viewchain.net/epro/auth/login',
+                              data: data);
+                          // 转译结果
+                          var res = convert.jsonDecode(response.toString());
+                          if (res["status"] == 1) {
+                            Fluttertoast.showToast(
+                                msg: '登录成功',
+                                toastLength: Toast.LENGTH_LONG,
+                                gravity: ToastGravity.CENTER,
+                                timeInSecForIos: 2,
+                                backgroundColor: Colors.green,
+                                textColor: Colors.white);
+                          } else {
+                            Fluttertoast.showToast(
+                                msg: '登录失败',
+                                toastLength: Toast.LENGTH_LONG,
+                                gravity: ToastGravity.CENTER,
+                                timeInSecForIos: 2,
+                                backgroundColor: Colors.red,
+                                textColor: Colors.white);
+                          }
+                        }
+                      },
+                    )
+                  ],
+                ),
+              )),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
