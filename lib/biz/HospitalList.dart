45a2e95655153466d04f6ca466706f87b18f572e
苏震 <suzhen@viewhigh.com>
2020-01-12 23:03:23 +0800

    Service fee page and api
    Service fee page and api

--- lib/biz/HospitalList.dart
+++ lib/biz/HospitalList.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'dart:convert' as convert;
+import 'package:flutter_hello_word/common/Loading.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:flutter_hello_word/utils/storage_util.dart';
 
@@ -11,6 +12,8 @@ class HospitalList extends StatefulWidget {
 
 class _HospitalListState extends State<HospitalList> {
   List formList;
+  // 定义全局 context; 为loading对象传递可用参数
+  BuildContext _context;
   @override
   void initState() {
     super.initState();
@@ -27,13 +30,14 @@ class _HospitalListState extends State<HospitalList> {
       // 获取请求token
       String token = await StorageUtil.getStringItem('token');
       Options options = Options(headers: {"authorization": token});
+      Loading loading = Loading(_context);
+      loading.show();
       Dio dio = new Dio();
-      // parameter 1: request path
-      // parameter 2: request parameters
       response = await dio.get(
-          'https://epro-op.test.viewchain.net/opapi/api/statistic/bigscreen?nowT=1578551906215',
+          'https://epro-op.test.viewchain.net/opapi/api/statistic/bigscreen?nowT=${new DateTime.now()}',
           options: options);
       var res = convert.jsonDecode(response.toString());
+      loading.close();
       if (res['status'] == 1) {
         return res;
       } else {
@@ -49,13 +53,13 @@ class _HospitalListState extends State<HospitalList> {
       print(e);
     }
   }
+
   Widget _buildList() {
     List<Widget> tiles = [];
 //    List<Widget> tiles = new List<Widget>();
     Widget content; // 单独的一个widget组件,返回需要生成的内容
     if (this.formList != null) {
       for (var item in this.formList) {
-        print(item);
         tiles.add(new ListTile(
           leading: Icon(Icons.local_hospital, size: 40, color: Colors.red),
           title: Text(item["name"]),
@@ -76,39 +80,19 @@ class _HospitalListState extends State<HospitalList> {
         children: tiles,
       );
       return content;
-    } else {
-      tiles.add(new Center(
-        child: Container(
-            margin: const EdgeInsets.all(50.0),
-            color: Colors.blueGrey,
-            child: Text('No data found!')),
-      ));
-      tiles.add(new Container(
-        child: RaisedButton(
-          child: Text('返回'),
-          color: Colors.blue,
-          textColor: Colors.white,
-          onPressed: () {
-            Navigator.pop(context, '返回登录');
-          },
-        ),
-      ));
-      content = new Column(
-        children: tiles,
-      );
-      return content;
     }
   }
+
   @override
   Widget build(BuildContext context) {
+    this._context = context;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('医院列表'),
-        centerTitle: true,
-      ),
-      body: new SingleChildScrollView(
-        child: _buildList(),
-      )
-    );
+        appBar: AppBar(
+          title: Text('医院列表'),
+          centerTitle: true,
+        ),
+        body: new SingleChildScrollView(
+          child: _buildList(),
+        ));
   }
 }
--- lib/biz/ServiceFee.dart
+++ lib/biz/ServiceFee.dart
@@ -1,27 +1,115 @@
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'dart:convert' as convert;
+import 'package:flutter_hello_word/common/Loading.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:flutter_hello_word/utils/storage_util.dart';
 
-class ServiceFee extends StatelessWidget {
+class ServiceFee extends StatefulWidget {
+  @override
+  _ServiceFeeState createState() => _ServiceFeeState();
+}
+
+class _ServiceFeeState extends State<ServiceFee> {
+  List formList;
+  // 定义全局 context; 为loading对象传递可用参数
+  BuildContext _context;
+  @override
+  void initState() {
+    super.initState();
+    getData().then((res) {
+      setState(() {
+        formList = res['result']['data'].toList();
+      });
+    });
+  }
+
+  Future getData() async {
+    try {
+      Response response;
+      // 获取请求token
+      String token = await StorageUtil.getStringItem('token');
+      Options options = Options(headers: {"authorization": token});
+      Loading loading = Loading(_context);
+      loading.show();
+      Dio dio = new Dio();
+      var data = {
+        'status':[1,2,3,7],
+        'pagination':{
+          'pageSize':500,
+          'pageNo':1
+        }
+      };
+      response = await dio.post(
+          'https://epro-op.test.viewchain.net/opapi/api/serviceFee/getServiceFeeList',
+          options: options,data:data);
+      print('response');
+      print(response);
+      var res = convert.jsonDecode(response.toString());
+      loading.close();
+      if (res['status'] == 1) {
+        return res;
+      } else {
+        Fluttertoast.showToast(
+            msg: res['msg'],
+            toastLength: Toast.LENGTH_LONG,
+            gravity: ToastGravity.CENTER,
+            timeInSecForIos: 1,
+            backgroundColor: Colors.red,
+            textColor: Colors.white);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Widget _buildList() {
+    List<Widget> tiles = [];
+    final pays = [
+      {'name':'未设置'},
+      {'name':'线上付款'},
+      {'name':'线下付款'},
+      {'name':'OK'}
+    ];
+//    List<Widget> tiles = new List<Widget>();
+    Widget content; // 单独的一个widget组件,返回需要生成的内容
+    if (this.formList != null) {
+      for (var item in this.formList) {
+        print(item);
+        tiles.add(new ListTile(
+//          isThreeLine: true,
+          leading: item['status'] == 3 ? Icon(Icons.tag_faces, size: 30, color: Colors.green) : Icon(Icons.add_alert, size: 30, color: Colors.orangeAccent),
+          title: Text('${item["feeNo"]} -- ${pays[item['payMethod']]['name']}'),
+          subtitle: Text('${item['supplierName']} : ${item['feeAmount']}元'),
+        ));
+      }
+      tiles.add(new Container(
+        child: RaisedButton(
+          child: Text('返回'),
+          color: Colors.blue,
+          textColor: Colors.white,
+          onPressed: () {
+            Navigator.pop(context, '返回登录');
+          },
+        ),
+      ));
+      content = new Column(
+        children: tiles,
+      );
+      return content;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    this._context = context;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('收费管理'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: new Column(
-          children: <Widget>[
-            RaisedButton(
-              child: Text('返回'),
-              color: Colors.blue,
-              textColor: Colors.white,
-              onPressed: (){
-                Navigator.pop(context,'go home');
-              },
-            )
-          ],
+        appBar: AppBar(
+          title: Text('收费列表'),
+          centerTitle: true,
         ),
-      ),
-    );
+        body: new SingleChildScrollView(
+          child: _buildList(),
+        ));
   }
 }
